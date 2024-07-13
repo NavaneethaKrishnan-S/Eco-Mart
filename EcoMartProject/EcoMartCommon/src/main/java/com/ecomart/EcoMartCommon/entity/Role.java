f9dac5a139eaeb52131ab8b15bc498251e193a53
Navaneethakrishnan-S <naveensaravana8@gmail.com>
2024-07-13 11:38:33 +0530

    Initial commit of EcoMartCommon.
    This contains common factors of frontend and backend of ecomart.

---
+++ EcoMartProject/EcoMartCommon/src/main/java/com/ecomart/EcoMartCommon/entity/Role.java
@@ -0,0 +1,39 @@
+package com.ecomart.EcoMartCommon.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 150, nullable = false)
+    private String description;
+
+    public Role(Integer id) {
+        this.id = id;
+    }
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
