ebc6f2d9abdc7bd9d0a14b8c370c83d307c7d5ed
Navaneethakrishnan-S <naveensaravana8@gmail.com>
2024-07-13 11:43:29 +0530

    Initial commit of EcoMartFrontEnd.
    This contains front end code for the ecomart project.

---
+++ EcoMartProject/EcoMartWebParent/EcoMartFrontEnd/src/main/java/com/ecomart/EcoMartFrontEnd/EcoMartFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.ecomart.EcoMartFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMartFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMartFrontEndApplication.class, args);
+	}
+
+}
