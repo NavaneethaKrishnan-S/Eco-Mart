3ac6fa1b95e9f9b5240a8bd5c10374b01a0e3d14
Navaneethakrishnan-S <naveensaravana8@gmail.com>
2024-11-28 11:21:34 +0530

    implement user data management with validations
    - Added functionality to handle user updates, including password management.
    - Preserved existing passwords when not provided during updates.
    - Added password encoding for both new and updated users.
    - Enhanced code readability and validation checks.

--- EcoMartProject/EcoMartWebParent/EcoMartBackEnd/src/main/java/com/ecomart/EcoMartBackEnd/service/UserService.java
+++ EcoMartProject/EcoMartWebParent/EcoMartBackEnd/src/main/java/com/ecomart/EcoMartBackEnd/service/UserService.java
@@ -6,7 +6,6 @@ import com.ecomart.EcoMartBackEnd.repository.UserRepository;
 import com.ecomart.EcoMartCommon.entity.Role;
 import com.ecomart.EcoMartCommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
